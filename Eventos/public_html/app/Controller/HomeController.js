d0ba0eb87897e9a2dd88102caf31c14aafdfb106
Sérgio <serrgio@outlook.com>
2016-07-05 13:19:44 -0300

    Primeiro commit Cliente_Eventos

---
+++ Eventos/public_html/app/Controller/HomeController.js
@@ -0,0 +1,5 @@
+/* global app */
+
+app.controller('HomeController', ["$scope", function ($scope) {
+
+}]);
---
+++ Eventos/public_html/app/app.js
@@ -0,0 +1,38 @@
+/* variáveis globais */
+var app = angular.module('Eventos', ['ui.router']);
+var webservice = "http://localhost:8084/webservice/";
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('home', {
+            url: '/',
+            templateUrl: 'app/views/home.html',
+            controller: 'HomeController'
+        })
+        .state('sobre', {
+            url: '/servicos',
+            templateUrl: 'app/views/servicos.html',
+            controller: 'SobreController'
+        })
+        .state('login', {
+            url: '/login',
+            templateUrl: 'app/views/login.html',
+            controller: 'loginController'
+        })
+        .state('cadpessoa', {
+            url: '/cadpessoa',
+            templateUrl: 'app/views/cadpessoa.html',
+            controller: 'cadpessoaController'
+        })
+        .state('cadcategoria', {
+            url: '/cadcategoria',
+            templateUrl: 'app/views/cadcategoria.html',
+            controller: 'cadcategoriaController'
+        })
+        .state('cadevento', {
+            url: '/cadevento',
+            templateUrl: 'app/views/cadevento.html',
+            controller: 'cadeventoController'
+        });        
+});
